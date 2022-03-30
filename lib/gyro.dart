b4ffbab81aa37e67db7bdca68e7b13c6d511d1d2
sambennion <bennionsamuel@gmail.com>
2022-03-29 23:09:54 -0600

    Add basic concept for gyro page

--- lib/gyro.dart
+++ lib/gyro.dart
@@ -6,13 +6,43 @@ class GyroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('iRecognize | Gyro Page'),
-        ),
-        body: const Center(
-          child: Text(
-            "Gyro",
+      appBar: AppBar(
+        title: const Text('iRecognize | Gyro Page'),
+      ),
+      body: Center(
+          child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(70),
+            child: Image(
+              image: AssetImage('assets/compass_icon.png'),
+              height: 100,
+              width: 100,
+            ),
           ),
-        ));
+          Container(
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(35)),
+                  color: Color.fromARGB(248, 202, 202, 206)),
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  children: const [
+                    Padding(padding: EdgeInsets.all(40)),
+                    Image(
+                      image: AssetImage('assets/brad-pic.jpg'),
+                      height: 50,
+                      width: 50,
+                    ),
+                    Padding(padding: EdgeInsets.all(20)),
+                    Text("Brad Pitt")
+                  ],
+                ),
+              )),
+          Padding(padding: EdgeInsets.all(30)),
+          ElevatedButton(onPressed: () {}, child: Text("View Bio"))
+        ],
+      )),
+    );
   }
 }
