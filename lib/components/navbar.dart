185cc80f99ac36d4ef116a67e9a8f9a71ad28652
sambennion <bennionsamuel@gmail.com>
2022-04-06 23:49:53 -0600

    Fix issue with homepage import

--- lib/components/navbar.dart
+++ lib/components/navbar.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:irecognize/components/sub_navbar.dart';
-import 'package:irecognize/home.dart';
 import 'package:irecognize/models/person_model.dart';
 import 'package:irecognize/pages/my_friends_page.dart';
 import 'package:irecognize/utils/constants.dart';
 import 'package:irecognize/utils/theme.dart';
 
+import '../pages/home_page.dart';
 import '../pages/person_page.dart';
 
 class Navbar extends StatelessWidget implements PreferredSizeWidget {
